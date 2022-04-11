fa0ef5f8b93cfd78a42edbca4c6790f05c08a273
SteveDutch <steholl@posteo.org>
2022-04-11 10:30:47 +0200

    brighten up

--- stefan-assignment-5/src/com/coderscampus/arraylist/CustomArrayList.java
+++ stefan-assignment-5/src/com/coderscampus/arraylist/CustomArrayList.java
@@ -3,17 +3,16 @@ package com.coderscampus.arraylist;
 import java.util.Arrays;
 
 public class CustomArrayList<T> implements CustomList<T> {
+	
 	Object[] items = new Object[10];
-	
+
 	private int place;
-	private int size = items.length; 
-	
+	private int size = items.length;
+
 	@Override
 	public boolean add(T item) {
-		// TODO Auto-generated method stub
-		System.out.println("I'm in add, index " + place + " it's round " + (place+1));
 		if (place == items.length - 1) {
-			size = size *2;
+			size = size * 2;
 			items = Arrays.copyOf(items, size);
 		}
 		items[place] = item;
@@ -23,18 +22,13 @@ public class CustomArrayList<T> implements CustomList<T> {
 
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
-		System.out.println("I'm in getSize");
-		System.out.println(items.length);
-		return place ;
+		return place;
 	}
 
 	@Override
 	public T get(int index) {
-		// TODO Auto-generated method stub
-		System.out.println("I'm in get");
 		T output = (T) items[index];
 		return output;
 	}
-	
+
 }
--- stefan-assignment-5/src/com/coderscampus/arraylist/CustomListApplication.java
+++ stefan-assignment-5/src/com/coderscampus/arraylist/CustomListApplication.java
@@ -3,21 +3,17 @@ package com.coderscampus.arraylist;
 public class CustomListApplication {
 
 	public static void main(String[] args) {
-		CustomList<String> myCustomList = new CustomArrayList<>();
 		
-		for (int j= 1; j <= 111; j++) {
-			String elem ="element " + String.valueOf(j);
-//			System.out.println(elem);
+		CustomList<String> myCustomList = new CustomArrayList<>();
+
+		for (int j = 1; j <= 111; j++) {
+			String elem = "element " + String.valueOf(j);
 			myCustomList.add(elem);
 		}
-		
-		for (int i=0; i<myCustomList.getSize(); i++) {
-		    System.out.println(myCustomList.get(i));
-		    
-		    System.out.println(i);
-		    System.out.println("size = " + myCustomList.getSize());
+
+		for (int i = 0; i < myCustomList.getSize(); i++) {
+			System.out.println(myCustomList.get(i));
 		}
-		
 
 	}
 
